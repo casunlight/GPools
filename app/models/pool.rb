4b4b8856dadb096849a6ab99a207959e2cbf68b0
bradwheel <bradley.m.wheel@gmail.com>
2012-11-21 14:57:55 -0500

    create description method

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -2,5 +2,9 @@ class Pool < ActiveRecord::Base
 
    attr_accessible :title, :latitude, :longitude, :address, :ind_out, :size, :gmaps
 
+   def description
+    "#{self.title} \n #{self.address} \n #{self.ind_out} \n #{self.size}"
+   end
+
 
 end
