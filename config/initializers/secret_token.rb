35c5b1c192b2c8d60c1b41edc40d9c9ea4e0a734
bradwheel <bradley.m.wheel@gmail.com>
2012-11-21 12:49:21 -0500

    create repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPool::Application.config.secret_token = 'cf298d6c9b33b049158ebdf71f95241003e263e824a8bb83900eaacd0ff9bd0c92c22308fab9e5d485cf83c1943c84949cc3fb24d0e3bc05ff63c1bfbffdb505'
